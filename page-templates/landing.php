d1ed47091b1d6bbb7d924f0c9128a90ad5866ea9
Andrei Chira <andreichira777@gmail.com>
2021-04-15 22:34:53 +0300

    landing footer

--- page-templates/landing.php
+++ page-templates/landing.php
@@ -54,7 +54,7 @@ remove_theme_support( 'genesis-menus' );
 // Removes site footer elements.
 remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
 remove_action( 'genesis_footer', 'genesis_do_footer' );
-remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
+//remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
 
 // Runs the Genesis loop.
 genesis();
