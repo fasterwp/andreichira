3ffac4dd434c72802c8c329fa92b2470cc487d12
Andrei Chira <andreichira777@gmail.com>
2021-06-06 01:28:48 +0300

    new base

--- page-templates/landing.php
+++ page-templates/landing.php
@@ -52,9 +52,9 @@ remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
 remove_theme_support( 'genesis-menus' );
 
 // Removes site footer elements.
-//remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
-//remove_action( 'genesis_footer', 'genesis_do_footer' );
-//remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
+remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
 
 // Runs the Genesis loop.
 genesis();
